ad8ac700cabed66850b31160975c323d72d46e2b
Giancarlo Matute <giancarlomatute99@gmail.com>
2022-06-01 00:05:31 -0400

    commit tree 184c4d532960bb939da242f4161698216dd4dff8

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// Write your solution in this file!
+let employee = {name: "gian", streetAdress: "123rd"}
+function updateEmployeeWithKeyAndValue(employee, key, value){
+    // employee[key] = value;
+    const clone = {...employee, [key]: value}
+    return clone
+}
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value){
+    employee[key] = value;
+    return employee
+}
+function  deleteFromEmployeeByKey(employee, key){
+    const clone = {...employee}
+    delete clone[key]
+    return clone
+}
+function destructivelyDeleteFromEmployeeByKey(employee, key){
+    delete employee[key]
+    return employee
+}
